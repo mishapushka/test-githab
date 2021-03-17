646202571d226e4ef47e416762013817f400ca73
Mikhail <bskfrost3@yandex.ru>
2021-03-18 02:07:02 +0700

    Initial Commit

---
+++ test githab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test githab
+//
+//  Created by mac on 18.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
